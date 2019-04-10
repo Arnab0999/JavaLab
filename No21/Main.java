6c29721fe727f5609a3afd29c17000465ef149b2
Arnab Sardar <44030498+Arnab0999@users.noreply.github.com>
2019-04-10 20:53:27 +0530

    Update Main.java

--- No21/Main.java
+++ No21/Main.java
@@ -1,26 +1,32 @@
 import java.util.*;
 class AddAll extends Thread {
 	double sum=0;
-	double[] array2 = new double[10];
-        Denominator d = new Denominator();
         public double returnResult()
         {
                 return sum;
         }
         @Override
-        synchronized public void run()
-        {
-		array2 = d.returnResult();
-                for(int i=0;i<10;i++)
-                {
-                    sum+=array2[i];
-		    System.out.print(array2[i]+" ");
-                }
+        synchronized public void run() {
+        	for(int i=0;i<10;i++)
+        	{
+        		Denominator d = new Denominator(i);
+        		d.start();
+        		try {
+					d.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+        		sum+=1/d.returnResult();
+        	}
         }
 }
 class Denominator extends Thread {
         double fact;
-        double[] array = new double[10];
+        int n;
+        Denominator(int x)
+        {
+        	n=x;
+        }
         public int returnsFactorial(int n)
         {
                 if(n==1||n==0)
@@ -28,27 +34,21 @@ class Denominator extends Thread {
                 else
                         return n*returnsFactorial(n-1);
         }
-        public double[] returnResult()
+        public double returnResult()
         {
-                return array;
+                return fact;
         }
         @Override
         synchronized public void run()
         {
-                for(int i=0;i<10;i++)
-                {
-                    array[i]=(double)returnsFactorial(i);
-		    System.out.print(array[i]+" ");
-                }
+                fact =(double)returnsFactorial(n);
         }
 }
 public class Main {
         public static void main(String args[]) throws Exception {
-                Denominator d = new Denominator();
                 AddAll a = new AddAll();
-		d.start();
-		d.join();
                 a.start();
+                a.join();
                 double sum = a.returnResult();
                 System.out.println("Sum:"+sum);
         }
